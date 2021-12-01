853ec75d66788c1c741210abf089af72974e9ac1
Ritesh Benjwal <riteshbenjwal7@gmail.com>
2021-12-01 20:43:15 +0530

    MULTIPLE_IMAGE_UPLOAD_HANDLE

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cloudinary = require("cloudinary").v2;
 const app = express();
 
 cloudinary.config({
-  cloud_name: "",
+  cloud_name: "riteshbenjwal",
   api_key: "",
   api_secret: "",
 });
@@ -33,19 +33,42 @@ app.post("/mypost", async (req, res) => {
   console.log(req.body);
   console.log(req.files);
 
-  let file = req.files.samplefile;
+  let result;
+  let imageArray = [];
 
-  let results = await cloudinary.uploader.upload(file.tempFilePath, {
-    folder: "users",
-  });
+  // case - multiple images
 
-  console.log(results);
+  if (req.files) {
+    for (let index = 0; index < req.files.samplefile.length; index++) {
+      let result = await cloudinary.uploader.upload(
+        req.files.samplefile[index].tempFilePath,
+        {
+          folder: "users",
+        }
+      );
 
-  details = {
+      imageArray.push({
+        public_id: result.public_id,
+        secure_url: result.secure_url,
+      });
+    }
+  }
+
+  // ### use case for single image
+  // let file = req.files.samplefile;
+  // result = await cloudinary.uploader.upload(file.tempFilePath, {
+  //   folder: "users",
+  // });
+
+  console.log(result);
+
+  let details = {
     firstname: req.body.firstname,
     lastname: req.body.lastname,
-    results: results,
+    result,
+    imageArray,
   };
+  console.log(details);
 
   res.send(details);
 });
